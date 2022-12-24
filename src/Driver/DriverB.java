995e501e55bd97439cea210876b8c775250e3e7e
Engel62 <sr.engel@bk.ru>
2022-12-24 22:24:19 +0300

    3 задание готово

--- src/Driver/DriverB.java
+++ src/Driver/DriverB.java
@@ -1,25 +1,51 @@
 package Driver;
 
 import Driver.Driver;
+import Transport.Competing;
+import Transport.Transport;
 
-public class DriverB extends Driver {
+public class DriverB <T extends Transport & Competing> extends Driver {
+
+    public DriverB() {
+        super("", true, 0);
+    }
 
     public DriverB(String fullName, boolean driversLicence, int drivingExperience) {
         super(fullName, driversLicence, drivingExperience);
     }
 
     @Override
-    public void startMoving() {
+    public String toString() {
+        return super.toString();
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    public void drive(T transport) {
+        System.out.println("Водитель: " + getFullName() + " управляет автомобилем: " + transport.getBrand() + " " +
+                transport.getModel() + " и будет участвовать в заезде");
+    }
+
+    @Override
+    public void startMoving() {
+        System.out.println("Водитель с водительскими правами категории В начинает движение");
     }
 
     @Override
     public void stopMoving() {
-
+        System.out.println("Водитель с водительскими правами категории В останавливается");
     }
 
     @Override
     public void refuelTheVehicle() {
-
+        System.out.println("Водитель с водительскими правами категории В заправляет транспортное средство");
     }
 }
